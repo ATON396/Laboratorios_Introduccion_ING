fbef24941222f3cd44156251e0ef264bfe5bbcb3
Manzanita123 <62077350+Manzanita123@users.noreply.github.com>
2020-03-11 15:49:23 -0500

    Update Problema_1.c

--- EV3_Lab3/Problema_1.c
+++ EV3_Lab3/Problema_1.c
@@ -4,7 +4,7 @@ task main()
 int leftMotor, rightMotor;
 
 moveMotorTarget(leftMotor, 720, 50);
-moveMotorTarget(rightMotor, 720, 50);
+moveMotorTarget(rightMotor, 720, 60);
 waitUntilMotorStop(leftMotor);
 waitUntilMotorStop(rightMotor);
 }
